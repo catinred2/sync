38d8d889b9e5cee1ac01e659ab66d66b9af1796d
idada <bg5sbk@gmail.com>
2014-12-08 20:50:17 +0800

    fix bug

--- deadlock.go
+++ deadlock.go
@@ -56,7 +56,7 @@ func (m *RWMutex) RUnlock() {
 
 var (
 	globalMutex = new(sync.Mutex)
-	waitingList = make(map[int32]*waiting)
+	waitingList = make(map[int32]*lockUsage)
 	titleStr    = []byte("[DEAD LOCK]\n")
 	goStr       = []byte("goroutine ")
 	waitStr     = []byte(" wait")
@@ -66,101 +66,105 @@ var (
 	lineStr     = []byte{'\n'}
 )
 
-type waiting struct {
-	monitor     *monitor
-	mode        byte
-	holder      int32
-	holderStack debug.StackInfo
+type lockUsage struct {
+	monitor *monitor
+	mode    byte
+	goid    int32
+	stack   debug.StackInfo
 }
 
 type monitor struct {
 	holders *list.List
 }
 
-func (m *monitor) wait(mode byte) *waiting {
+func (m *monitor) wait(mode byte) *lockUsage {
 	globalMutex.Lock()
 	defer globalMutex.Unlock()
 
-	waitInfo := &waiting{m, mode, goid.Get(), debug.StackTrace(3, 0)}
-	waitingList[waitInfo.holder] = waitInfo
+	waitInfo := &lockUsage{m, mode, goid.Get(), debug.StackTrace(3, 0)}
+	waitingList[waitInfo.goid] = waitInfo
 
 	if m.holders == nil {
 		m.holders = list.New()
 	}
 
-	m.verify(mode, []*waiting{waitInfo})
+	m.diagnose(mode, []*lockUsage{waitInfo})
 
 	return waitInfo
 }
 
-func (m *monitor) verify(mode byte, waitLink []*waiting) {
+func (m *monitor) diagnose(mode byte, waitLink []*lockUsage) {
 	for i := m.holders.Front(); i != nil; i = i.Next() {
-		holder := i.Value.(*waiting)
+		holder := i.Value.(*lockUsage)
 		if mode != 'r' || holder.mode != 'r' {
 			// deadlock detected
-			if holder.holder == waitLink[0].holder {
-				buf := new(bytes.Buffer)
-				buf.Write(titleStr)
-				for i := 0; i < len(waitLink); i++ {
-					buf.Write(goStr)
-					buf.WriteString(strconv.Itoa(int(waitLink[i].holder)))
-					buf.Write(waitStr)
-					if waitLink[i].mode == 'w' {
-						buf.Write(writeStr)
-					} else {
-						buf.Write(readStr)
-					}
-					buf.Write(lineStr)
-					buf.Write(waitLink[i].holderStack.Bytes("  "))
-
-					// lookup waiting for who
-					n := i + 1
-					if n == len(waitLink) {
-						n = 0
-					}
-					waitWho := waitLink[n]
-
-					for j := waitLink[i].monitor.holders.Front(); j != nil; j = j.Next() {
-						waitHolder := j.Value.(*waiting)
-						if waitHolder.holder == waitWho.holder {
-							buf.Write(goStr)
-							buf.WriteString(strconv.Itoa(int(waitHolder.holder)))
-							buf.Write(holdStr)
-							if waitHolder.mode == 'w' {
-								buf.Write(writeStr)
-							} else {
-								buf.Write(readStr)
-							}
-							buf.Write(lineStr)
-							buf.Write(waitHolder.holderStack.Bytes("  "))
-							break
-						}
-					}
-				}
-				panic(DeadlockError(buf.String()))
+			if holder.goid == waitLink[0].goid {
+				deadlockPanic(waitLink)
 			}
 			// the lock holder is waiting for another lock
-			if waitInfo, exists := waitingList[holder.holder]; exists {
-				waitInfo.monitor.verify(waitInfo.mode, append(waitLink, waitInfo))
+			if waitInfo, exists := waitingList[holder.goid]; exists {
+				waitInfo.monitor.diagnose(waitInfo.mode, append(waitLink, waitInfo))
 			}
 		}
 	}
 }
 
-func (m *monitor) using(waitInfo *waiting) {
+func (m *monitor) using(waitInfo *lockUsage) {
 	globalMutex.Lock()
 	defer globalMutex.Unlock()
 
-	delete(waitingList, waitInfo.holder)
+	delete(waitingList, waitInfo.goid)
 	m.holders.PushBack(waitInfo)
 }
 
 func (m *monitor) release(mode byte) {
-	holder := goid.Get()
+	id := goid.Get()
 	for i := m.holders.Back(); i != nil; i = i.Prev() {
-		if info := i.Value.(*waiting); info.holder == holder && info.mode == mode {
+		if info := i.Value.(*lockUsage); info.goid == id && info.mode == mode {
 			m.holders.Remove(i)
 			break
 		}
 	}
+}
+
+func deadlockPanic(waitLink []*lockUsage) {
+	buf := new(bytes.Buffer)
+	buf.Write(titleStr)
+	for i := 0; i < len(waitLink); i++ {
+		buf.Write(goStr)
+		buf.WriteString(strconv.Itoa(int(waitLink[i].goid)))
+		buf.Write(waitStr)
+		if waitLink[i].mode == 'w' {
+			buf.Write(writeStr)
+		} else {
+			buf.Write(readStr)
+		}
+		buf.Write(lineStr)
+		buf.Write(waitLink[i].stack.Bytes("  "))
+
+		// lookup waiting for who
+		n := i + 1
+		if n == len(waitLink) {
+			n = 0
+		}
+		waitWho := waitLink[n]
+
+		for j := waitLink[i].monitor.holders.Front(); j != nil; j = j.Next() {
+			waitHolder := j.Value.(*lockUsage)
+			if waitHolder.goid == waitWho.goid {
+				buf.Write(goStr)
+				buf.WriteString(strconv.Itoa(int(waitHolder.goid)))
+				buf.Write(holdStr)
+				if waitHolder.mode == 'w' {
+					buf.Write(writeStr)
+				} else {
+					buf.Write(readStr)
+				}
+				buf.Write(lineStr)
+				buf.Write(waitHolder.stack.Bytes("  "))
+				break
+			}
+		}
+	}
+	panic(DeadlockError(buf.String()))
 }
